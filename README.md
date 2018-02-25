fa5a22bf16e8faa700c26f543fd04f67e1b96aee
Unknown <eveiga@msn.com>
2018-02-25 03:37:47 -0300

    Interr de botao nao ta funcionando ainda
    mas eu juro que de amanhã não passa

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-lab3
+essa caralha é pra ser um controle de SCR via arduino.
+
+Por motivos de cagalhão e preguiçoso, a frequencia é fixa em 50hz pra ser gerado 3 sinais distintos de 5vpp.
+
+os timers tão setados pra 100hz por nyquist (mentira, é pq é a unica frequencia harmonica que eu consigo usar os 3 timers do 328p)
+nesses 100hz cada um só vai contar em pulso alternado
+
+cada interr do timer vai somar num contador de cada fase
+quando o contador da fase for igual ou maior que a leitura do ADC que é o caralho do angulo de corte
+vai mandar o sinal pro MOC3020 PQ EU NÃO TENHO TEMPO PRA USAR UM TRANSFORMADOR DE PULSO
+
+mt tempo gasto procrasturbando
+
+
+o MOC3020 nesse exato momento tá sendo representado por outros 3 leds que tem vontade e desejos proprios
+
+na real eu n sei bem qq ta acontecendo
+
+
+
+.... mass eu faço alguma ideia
+
+
+primeiro prototipo vai trocar os optoacoplador 4n25 por push button pra ficar mais facil de discernir e não precisar de um fucking gerador de função
+inclusive to aceitando
--- SCR_prototipo_skeleton.c
+++ SCR_prototipo_skeleton.c
@@ -2,161 +2,165 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <avr/interrupt.h>
+#include <avr/io.h>
 
 volatile int  Rglob=0;
 volatile int  Sglob=0;
 volatile int  Tglob=0;
-volatile bool Rcnt=true;
-volatile bool Scnt=true;
-volatile bool Tcnt=true;
-volatile int angle_cutoff = 0;
+volatile int cutoff = 0;
+volatile int angulo = 0;
+volatile bool Rtrig, Strig, Ttrig = false;
 
 void setup(){
-	cli();
-	Serial.begin(9600); 
-	DDRB = 0xDF; //R INPUT PINB5 DIGITAL PIN 13 / VERDE
-	DDRC = 0xFE; //S INPUT PINC0 ANALOG INPUT 0 / VERMELHO
-	DDRD = 0x7F; //T INPUT PIND7 DIGITAL PIN 7 / AMARELO
+  
+  Serial.begin(9600); 
+  
+	DDRB  = 0x00; //R INPUT PINB5 DIGITAL PIN 13 / VERDE
+	DDRB  = (1 << PORTB2) | (1 << PORTB3) | (1 << PORTB4);
+	PORTB |= (1 << PORTB5);
+  
+	DDRC = 0x00; //S INPUT PINC0 ANALOG INPUT 0 / VERMELHO
+	PORTC |= (1 << PORTC0);
+  
+	DDRD = 0x00; //T INPUT PIND7 DIGITAL PIN 7 / AMARELO
+	PORTD |= (1 << PORTD7);
 
-	//OUTROS PINOS COMO OUTPUT
-	
-	/*
-	3 timers ajustados pra 100hz pra contar até 1024 cada um 
-	em cada interr. Em cada interr, o valor é comparado a leitura do ADC
-	
-	*/
+  //OUTROS PINOS COMO OUTPUT
 
-	//TIMR0. fase R
+  //interr de pino
+	EICRA = 0x05;
+	PCICR |= (1 << PCIE2) | (1 << PCIE1) | (1 << PCIE0);
+	PCMSK2 |= (1 << PCINT23);
+	PCMSK1 |= (1 << PCINT8);
+	PCMSK0 |= (1 << PCINT5);
+  
+  /*
+  3 timers ajustados pra 100hz pra contar até 1024 cada um 
+  em cada interr. Em cada interr, o valor é comparado a leitura do ADC
+  
+  */
+
+  //TIMR0. fase R
 	TCCR0A = 0;
 	TCCR0B = 0;
 	TCNT0 = 0;    // 100.16025641025641 Hz (16000000/((155+1)*1024))
-	OCR0A = 155;  // CTC
-	TCCR0A |= (1 << WGM01);  // Prescaler 1024
-	TCCR0B |= (1 << CS02) | (1 << CS00);  // Output Compare Match A Interrupt Enable
-	TIMSK0 |= (1 << OCIE0A);
+	OCR0A = 155;  
+	TCCR0A |= (1 << WGM01); // CTC
+	TCCR0A = 0x00;// afeta o disparo do timer, eu só quero disparar na interrupção, portanto
+	TIMSK0 |= (1 << OCIE0A);// Output Compare Match A Interrupt Enable
 
-	//TIMR1. fase S
+  //TIMR1. fase S
 	TCCR1A = 0;
 	TCCR1B = 0;
 	TCNT1 = 0;
 	OCR1A = 624;              // 100 Hz (16000000/((624+1)*256))
-	TCCR1B |= (1 << WGM12);   // CTC
-	TCCR1B |= (1 << CS12);    // Prescaler 256
+	CCR1B |= (1 << WGM12);   // CTC. Prescaler 256, setado na interr
 	TIMSK1 |= (1 << OCIE1A);  // Output Compare Match A Interrupt Enable
 
-	//TIMR2. fase T
+  //TIMR2. fase T
 	TCCR2A = 0;
 	TCCR2B = 0;
 	TCNT2 = 0;
 	OCR2A = 155;    // 100.16025641025641 Hz (16000000/((155+1)*1024))  
-	TCCR2A |= (1 << WGM21);// CTC  
-	TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20);// Prescaler 1024  
+	TCCR2A |= (1 << WGM21);// CTC  . Prescaler 1024  interr
 	TIMSK2 |= (1 << OCIE2A);// Output Compare Match A Interrupt Enable
  
-
+  sei();
 }
 
 void loop(){
-	
-	int cutoff = analogRead(A5);
-	
-	/*
-	 esse trecho vai ser comentado fora com o gerador de sinais pq tipo ele vai
-	chavear o opto a cada 1024 ticks ou menos
-	 */
+  
+	cutoff = analogRead(A5);
+	angulo = cutoff * 0.1578125;
+  
+  /*
+		esse trecho vai ser comentado fora com o gerador de sinais pq tipo ele vai
+		chavear o opto a cada 1024 ticks ou menos
 
-	if (Rglob >= 1024)
-	{
+   */
+
+	if (Rglob >= 1023){
 		Rglob = 0;
+		PORTB |= (0 << PB2);
 	}
 
-	if (Sglob >= 1024)
-	{
+	if (Sglob >= 1023){
 		Sglob = 0;
+		PORTB |= (1 << PB3);
 	}
 
-	if (Tglob >= 1024)
-	{
+	if (Tglob >= 1023){
 		Tglob = 0;
+		PORTB |= (1 << PB4);
 	}
 
-	Serial.println("Angulo atual, %d" &cutoff);
-	Serial.print("\t R = %d", &Rglob);
-	Serial.print("\t S = %d", &Sglob);
-	Serial.print("\t T = %d", &Tglob);
-
-	// compara o angulo atual com o angulo de corte e dispara o MOC
-	if (Rglob == cutoff)
-	{
+	Serial.println("");
+	Serial.print("POT = ");
+	Serial.print(cutoff);
+  
+	Serial.print("\t Angulo de corte = ");
+	Serial.print(angulo);
+	Serial.print("\t R = ");
+	Serial.print(TCNT0);
+	Serial.print("\t S = ");
+	Serial.print(TCNT1);
+	Serial.print("\t T = ");
+	Serial.print(TCNT2);
+  
+  // compara o angulo atual com o angulo de corte e dispara o MOC
+	
+	if (Rglob >= cutoff){
 		PORTB |= (1 << PB2); 
 	}
 
-	if (Sglob == cutoff)
-	{
+	if (Sglob >= cutoff) {
 		PORTB |= (1 << PB3); 
 	}
 
-	if (Tglob == cutoff)
-	{
+	if (Tglob >= cutoff) {
 		PORTB |= (1 << PB4); 
 	}
 
-
+ 
 }
 
 
 // IRQs do tipo PCINT representam as interrupções do optoacoplador.
 // inclusive precisa resetar o timer
 ISR(PCINT0_vect){ //INTERR fase R.
-
-	Serial.println("FASE R TRIGADA")
-	TCNT0 = 0;
+	Rtrig = true;	
+	TCNT0 = 0x00;
+	TCCR0B |= (1 << CS02) | (1 << CS00);  //prescaler
 	PORTB |= (0 << PB2);
+	Rglob = 0;
 }
 
 ISR(PCINT1_vect){ //INTERR fase S
-
-	Serial.println("FASE S TRIGADA")
-	TCNT1 = 0;
+	Rtrig = true;	
+	TCCR1B |= (1 << CS12);    // Prescaler 256
+	TCNT1 = 0x00;
+	Sglob = 0;  
 	PORTB |= (0 << PB3);
 }
 
 ISR(PCINT2_vect){ //INTERR fase T. A parte de desativar o sinal do moc talvez precise ficar inclusa
-
-	Serial.println("FASE T TRIGADA")
-	TCNT2 = 0;
-	PORTB |= (0 << PB4);
+	Rtrig = true;	
+	TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20);
+	TCNT2 = 0x00;
+	Tglob = 0;
 }
 
 ISR(TIMER0_COMPA_vect){
-
-	Rcnt = ~Rcnt;
-	
-	if (Scnt == true)
-	{
-		++Rglob;
-	}
-
+	Rglob++;
+  	Rtrig = false;
 }
 
 ISR(TIMER1_COMPA_vect){
-
-	Scnt = ~Scnt;
-	
-	if (Scnt == true)
-	{
-		++Sglob;
-	}
-
+ 	Sglob++;
+	Strig = false;
 }
 
 ISR(TIMER2_COMPA_vect){
-
-	Tcnt = ~Tcnt;
-	if (Tcnt == true)
-	{
-		++Tglob;
-	}
-
-
+	Tglob++;
+  	Ttrig = false;
 }
